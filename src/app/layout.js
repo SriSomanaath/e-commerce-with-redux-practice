8071f521c73c7afa6588a77acd8ae63b8544d630
SriSomanaath <govindalapudisrinath@gmail.com>
2023-12-26 17:11:40 +0530

    final commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
 import { Inter } from 'next/font/google'
+import Navbar from '@/componet/Navbar'
+import Prvider from '@/Redux/Prvider'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -11,7 +13,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Prvider>
+        <Navbar/>
+        {children}
+        </Prvider>
+        </body>
     </html>
   )
 }
